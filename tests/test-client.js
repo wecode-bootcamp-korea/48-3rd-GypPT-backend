7d3082bb7bae183ddd380eb74a980c4c1c2f2064
정동민 <jeongdongmin@jeongdongmin-ui-MacBookAir.local>
2023-09-07 19:57:32 +0900

    feat: initial setting

---
+++ tests/test-client.js
@@ -0,0 +1,13 @@
+const dataSource = require('../api/models/dataSource');
+
+const truncateTables = async (tableList) => {
+  await dataSource.query(`SET FOREIGN_KEY_CHECKS=0`);
+
+  for (let table of tables) {
+    await dataSource.query(`TRUNCATE table ${table}`);
+  }
+
+  await dataSource.query(`SET FOREIGN_KEY_CHECKS=1`);
+};
+
+module.exports = { truncateTables };
