df4475e0e7c9d8c3721592789f2c5585a2f62f4c
dodoyoo <dong15903@daum.net>
2023-09-13 20:20:38 +0900

    add : post consultant 2

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const router = express.Router();
+
 const { userRouter } = require('./userRouter');
 const { trainerRouter } = require('./trainerRouter');
 const { managementRouter } = require('./managementRouter');
+const { consultantRouter } = require("./consultantRouter")
 
-const router = express.Router();
 
 router.use('/user', userRouter);
 router.use('/trainers', trainerRouter);
 router.use('/custom', managementRouter);
+router.use("/consultant", consultantRouter);
 
-module.exports = { router };
+
+
+
+module.exports = { router }
