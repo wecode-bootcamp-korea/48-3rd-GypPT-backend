a1d7336ca211dccd5833713b66857748cbdd9710
dodoyoo <dong15903@daum.net>
2023-09-15 15:18:14 +0900

    add: modify threads table 2

--- db/migrations/20230915053736_modify.sql
+++ db/migrations/20230915053736_modify.sql
@@ -1,5 +1,7 @@
 -- migrate:up
-ALTER TABLE threads ADD trainer_id INT NOT NULL
+ALTER TABLE threads ADD trainer_profile_id INT NULL;
+ALTER TABLE threads ADD FOREIGN KEY(trainer_profile_id) REFERENCES trainer_profiles(id);
 
 -- migrate:down
+
 
