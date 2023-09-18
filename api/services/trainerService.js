99a80f56efff7a71f5b8c9031ca20b140802ab5a
suyeongs <suyeongs@gmail.com>
2023-09-18 19:50:31 +0900

    refactor: Move offset and limit calculation logic from controller layer to service layer

--- api/services/trainerService.js
+++ api/services/trainerService.js
@@ -1,6 +1,9 @@
 const trainerDao = require('../models/trainerDao');
 
-const getTrainerList = async (offset, pageSize) => {
+const getTrainerList = async (page, limit) => {
+  const pageSize = parseInt(limit);
+  const offset = (page - 1) * pageSize;
+
   return await trainerDao.getTrainerList(offset, pageSize);
 };
 
