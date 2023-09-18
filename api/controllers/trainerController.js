f724429715ba629f7d0dfe91f08e54122081a351
suyeongs <suyeongs@gmail.com>
2023-09-18 19:50:31 +0900

    refactor: Move offset and limit calculation logic from controller layer to service layer

--- api/controllers/trainerController.js
+++ api/controllers/trainerController.js
@@ -3,10 +3,8 @@ const { catchAsync } = require('../utils/error');
 
 const getTrainerList = catchAsync(async (req, res) => {
   const { page = 1, limit = 5 } = req.query;
-  const pageSize = parseInt(limit);
-  const offset = (page - 1) * pageSize;
 
-  const trainerList = await trainerService.getTrainerList(offset, pageSize);
+  const trainerList = await trainerService.getTrainerList(page, limit);
 
   res.status(200).json({ data: trainerList });
 });
