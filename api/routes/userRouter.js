e81dadce707524794ac2ec26ca6526c399cbbbf2
김시연 <kimsiyeon@MacBook-Air.local>
2023-09-19 10:48:37 +0900

    add : mypage

--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -7,4 +7,6 @@ const userRouter = express.Router();
 userRouter.post('/kakao-sign-in', userController.kakaoSignIn);
 userRouter.post('/kakao-sign-up', userController.kakaoSignUp);
 userRouter.post('/duplicate-nickname', userController.checkDuplicateNickname);
+userRouter.get('/mypage', loginRequired, userController.getMyPage);
+
 module.exports = { userRouter };
