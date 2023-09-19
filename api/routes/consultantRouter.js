4fada21ec89d32c4c3aa18f1f2c99e3dbe564d5b
dodoyoo <dong15903@daum.net>
2023-09-19 14:16:58 +0900

    add : delete consultant casecade

--- api/routes/consultantRouter.js
+++ api/routes/consultantRouter.js
@@ -6,5 +6,6 @@ const consultantRouter = express.Router();
 const { loginRequired } = require("../utils/auth");
 
 consultantRouter.post("/posts/upload", consultantController.createConsultant);
+consultantRouter.delete("/posts/:postId", consultantController.deleteConsultant);
 
 module.exports = { consultantRouter };
