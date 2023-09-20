c1e88ef817596bf7d6050daf69cfebaf51f2f75a
dodoyoo <dong15903@daum.net>
2023-09-20 11:24:04 +0900

    add : consultant list modify index & finish

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,13 +2,16 @@ const express = require('express');
 const { userRouter } = require('./userRouter');
 const { trainerRouter } = require('./trainerRouter');
 const { managementRouter } = require('./managementRouter');
+const { consultantRouter } = require('./consultantRouter');
+const { postRouter } = require('./postRouter');
+const { paymentRouter } = require('./paymentRouter');
 
 const router = express.Router();
 
 router.use('/trainers', trainerRouter);
 router.use('/custom', managementRouter);
-router.use("/consultant", consultantRouter);
-router.use('/users', userRouter);
+router.use('/consultant', consultantRouter);
+router.use('/user', userRouter);
 router.use('/community', postRouter);
 router.use('/payments', paymentRouter);
 
